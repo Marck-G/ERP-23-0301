728a08863125fc5df8dc4e7edd7904cf5e515b34
Marck Carrión Guzmán <mcarrion@kcramsolutions.com>
2023-03-02 14:41:29 +0100

    feat: creado modelo status

---
+++ src/models/status.js
@@ -0,0 +1,19 @@
+const { Model, DataTypes } = require("sequelize");
+
+
+class Status extends Model{}
+
+function init(sequelize){
+	Status.init({
+		status: {type: DataTypes.STRING, primaryKey: true},
+		desc: {type: DataTypes.STRING},
+	}, {
+		sequelize,
+		tableName: 'ERP_001T00I00'
+	});
+}
+
+
+module.exports = {
+	Status, init
+}
